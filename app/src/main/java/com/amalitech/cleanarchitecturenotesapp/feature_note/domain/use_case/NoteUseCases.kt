5961ce9d634c56f45415ed4c7c307c72cdb94d33
ben rapha <bensek73@gmail.com>
2022-10-10 15:07:05 +0200

    created use cases

---
+++ app/src/main/java/com/amalitech/cleanarchitecturenotesapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -0,0 +1,8 @@
+package com.amalitech.cleanarchitecturenotesapp.feature_note.domain.use_case
+
+import com.amalitech.cleanarchitecturenotesapp.feature_note.domain.model.Note
+
+data class NoteUseCases(
+    val getNoteUseCase: GetNotesUseCase,
+    val deleteNoteUseCase: DeleteNoteUseCase
+)
